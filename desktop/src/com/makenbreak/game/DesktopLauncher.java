ac722c7cd65e4e7366a2cfebd42bbcb3dda036dd
AlbusNotThuan <107522443+AlbusNotThuan@users.noreply.github.com>
2024-01-09 11:07:28 +0100

    Project Template Files

---
+++ desktop/src/com/makenbreak/game/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package com.makenbreak.game;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.makenbreak.game.MakeandBreak;
+
+// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setForegroundFPS(60);
+		config.setTitle("Make 'n Break");
+		new Lwjgl3Application(new MakeandBreak(), config);
+	}
+}
