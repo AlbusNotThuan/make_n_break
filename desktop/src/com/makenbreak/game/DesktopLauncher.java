52059f37179a99430be615dd02b6716f2f3af595
AlbusNotThuan <107522443+AlbusNotThuan@users.noreply.github.com>
2024-01-11 11:56:59 +0100

    Change initial window size setting

--- desktop/src/com/makenbreak/game/DesktopLauncher.java
+++ desktop/src/com/makenbreak/game/DesktopLauncher.java
@@ -1,5 +1,6 @@
 package com.makenbreak.game;
 
+import com.badlogic.gdx.Graphics;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 import com.makenbreak.game.MakeandBreak;
@@ -8,8 +9,11 @@ import com.makenbreak.game.MakeandBreak;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		Graphics.DisplayMode dm = Lwjgl3ApplicationConfiguration.getDisplayMode();
+		config.setWindowedMode(dm.width / 2, dm.height / 2);
 		config.setForegroundFPS(60);
 		config.setTitle("Make 'n Break");
 		new Lwjgl3Application(new MakeandBreak(), config);
+
 	}
 }
