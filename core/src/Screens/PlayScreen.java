27dbf5f948bae5cf52f87a8a547cc30bbb74ad3b
AlbusNotThuan <107522443+AlbusNotThuan@users.noreply.github.com>
2024-01-11 18:00:10 +0100

    Reload the project to include Freetype

--- core/src/Screens/PlayScreen.java
+++ core/src/Screens/PlayScreen.java
@@ -1,5 +1,6 @@
 package Screens;
 
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
@@ -7,18 +8,18 @@ import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
-import com.makenbreak.game.MakeandBreak;
+import com.makeandbreak.game.MakeAndBreak;
 
 public class PlayScreen implements Screen {
-    private MakeandBreak game;
+    private MakeAndBreak game;
     Texture texture;
     private OrthographicCamera gamecam;
     private Viewport gameport;
-    public PlayScreen(MakeandBreak game){
+    public PlayScreen(MakeAndBreak game){
         this.game = game;
-        texture = new Texture("testing.gif");
+        texture = new Texture("workinprogress.png");
         gamecam = new OrthographicCamera();
-        gameport = new FitViewport(800, 800, gamecam);
+        gameport = new FitViewport(1600 , 800, gamecam);
     }
     @Override
     public void show() {
@@ -27,15 +28,12 @@ public class PlayScreen implements Screen {
 
     @Override
     public void render(float delta) {
-        Gdx.gl.glClearColor(1, 0, 0, 1);
+        Gdx.gl.glClearColor(0, 1, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-
         game.batch.setProjectionMatrix(gamecam.combined);
-
         game.batch.begin();
-        game.batch.draw(texture, 0, 00);
+        game.batch.draw(texture , 0 ,0);
         game.batch.end();
-
     }
 
     @Override
