348722d4aefe9f74ead0e95df7d7f99c30bcdf02
AlbusNotThuan <107522443+AlbusNotThuan@users.noreply.github.com>
2024-01-12 19:30:48 +0100

    Reload the project to include Freetype

---
+++ core/src/com/makeandbreak/game/MakeAndBreak.java
@@ -0,0 +1,29 @@
+package com.makeandbreak.game;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.ScreenUtils;
+
+import Screens.PlayScreen;
+
+public class MakeAndBreak extends Game {
+	public SpriteBatch batch;
+	
+	@Override
+	public void create () {
+		batch = new SpriteBatch();
+		setScreen(new PlayScreen(this));
+	}
+
+	@Override
+	public void render () {
+		super.render();
+	}
+	
+	@Override
+	public void dispose () {
+
+	}
+}
