c9a535c5e6b9059049c427ef15b976161e1870ef
Le Huu Trinh Kha <trinhkhasake@gmail.com>
2024-01-18 04:31:47 +0700

    menu game

---
+++ desktop/src/com/makenbreak/game/DesktopLauncher.java
@@ -0,0 +1,17 @@
+package com.makenbreak.game;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.makenbreak.game.MakeNBreakGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setForegroundFPS(60);
+		config.setTitle("Make 'n' Break");
+		config.setWindowedMode(800, 600);
+		new Lwjgl3Application(new MakeNBreakGame(), config);
+	}
+}
+
+
