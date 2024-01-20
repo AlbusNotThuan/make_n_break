f1513141c307f35b184d5c8513699e2dc643d6c6
TrinhKhasake <92497591+TrinhKhasake@users.noreply.github.com>
2024-01-20 20:30:15 +0700

    Update DesktopLauncher.java

--- desktop/src/com/makenbreak/game/DesktopLauncher.java
+++ desktop/src/com/makenbreak/game/DesktopLauncher.java
@@ -10,6 +10,7 @@ public class DesktopLauncher {
 		config.setForegroundFPS(60);
 		config.setTitle("Make 'n' Break");
 		config.setWindowedMode(800, 600);
+		config.setResizable(false);
 		new Lwjgl3Application(new MakeNBreakGame(), config);
 	}
 }
