f7308c6790fc2576f1047cfff83acaea8f3d5429
Crooker <duyphuc722003@gmail.com>
2024-01-31 17:28:23 +0100

    change location of grid and colorselector

--- core/src/Screens/MainMenuScreen.java
+++ core/src/Screens/MainMenuScreen.java
@@ -2,6 +2,8 @@ package Screens;
 
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.audio.Music;
+import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.ScreenAdapter;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
@@ -27,6 +29,8 @@ public class MainMenuScreen extends ScreenAdapter {
     private BitmapFont font;
     private OrthographicCamera gamecam;
     private Viewport gameport;
+    private Music music;
+    private Sound clicksound;
     private Texture img= new Texture(Gdx.files.internal("anhgame.png"));;
     public MainMenuScreen(MakeAndBreak game) {
         this.game = game;
@@ -36,6 +40,15 @@ public class MainMenuScreen extends ScreenAdapter {
 
     @Override
     public void show() {
+        //music
+        music = Gdx.audio.newMusic(Gdx.files.internal("mainmenu_msc.mp3"));
+        music.setLooping(true);
+        music.setVolume(0.5f);
+        music.play();
+
+        //sound
+        clicksound=Gdx.audio.newSound(Gdx.files.internal("clicksound.mp3"));
+
         font = new BitmapFont();
         stage = new Stage(new ScreenViewport());
         Gdx.input.setInputProcessor(stage);
@@ -50,6 +63,8 @@ public class MainMenuScreen extends ScreenAdapter {
             @Override
             public void clicked(InputEvent event, float x, float y) {
                 try {
+                    music.stop();
+                    clicksound.play();
                     game.setScreen(new ClassicScreen((MakeAndBreak) game));
                 } catch (FileNotFoundException e) {
                     throw new RuntimeException(e);
@@ -60,6 +75,8 @@ public class MainMenuScreen extends ScreenAdapter {
         ruleButton.addListener(new ClickListener() {
             @Override
             public void clicked(InputEvent event, float x, float y) {
+                music.stop();
+                clicksound.play();
                 game.setScreen(new RuleScreen(game));
             }
         });
