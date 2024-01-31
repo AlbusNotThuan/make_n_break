2d74e584bddcdb59f6c0498996bfc86defc742ee
AlbusNotThuan <107522443+AlbusNotThuan@users.noreply.github.com>
2024-01-31 11:07:57 +0100

    project: bufferscreen

--- core/src/Screens/BufferScreen.java
+++ core/src/Screens/BufferScreen.java
@@ -26,30 +26,24 @@ public class BufferScreen implements Screen  {
     public BufferScreen(MakeAndBreak game){
         this.game = game;
         gamecam = new OrthographicCamera();
-        gameport = new FitViewport(game.WIDTH, game.HEIGHT, gamecam);
-        gamecam.setToOrtho(false, game.WIDTH, game.HEIGHT);
-        gamecam.translate(-game.WIDTH/2,-game.HEIGHT/2);
+        gameport = new FitViewport(1520 , 600, gamecam);
+        gamecam.setToOrtho(false, 1520,600);
+        gamecam.translate(-1520/2,-600/2);
         this.stage = new Stage(gameport, game.batch);
     }
     @Override
     public void show() {
         TextureAtlas atlas = new TextureAtlas(Gdx.files.internal("ready.atlas"));
         Skin skin = new Skin(Gdx.files.internal("ready.json"),atlas);
-        Gdx.input.setInputProcessor(stage);
 
 
         ImageButton.ImageButtonStyle letsbuildStyle = new ImageButton.ImageButtonStyle();
         letsbuildStyle = skin.get("letsbuild", ImageButton.ImageButtonStyle.class);
 
-        ImageButton.ImageButtonStyle readyStyle = new ImageButton.ImageButtonStyle();
-        readyStyle = skin.get("ready", ImageButton.ImageButtonStyle.class);
-
-        ImageButton readyButton = new ImageButton(readyStyle);
+        System.out.println(game.WIDTH);
         ImageButton letsbuildButton = new ImageButton(letsbuildStyle);
-        letsbuildButton.setSize(300,102);
-        readyButton.setSize(99*3,65*3);
-        readyButton.setPosition(game.WIDTH / 2 - readyButton.getWidth() / 2, game.HEIGHT / 2 - readyButton.getHeight() / 2 + letsbuildButton.getHeight()/2 + 10);
-        letsbuildButton.setPosition(game.WIDTH / 2 - letsbuildButton.getWidth() / 2, game.HEIGHT / 2 - letsbuildButton.getHeight() / 2);
+        letsbuildButton.setPosition(0,0);
+//        letsbuildButton.setPosition(game.WIDTH / 2 - letsbuildButton.getWidth() / 2, game.HEIGHT / 2 - letsbuildButton.getHeight() / 2);
         letsbuildButton.addListener(new ClickListener() {
             @Override
             public void clicked(InputEvent event, float x, float y) {
@@ -62,13 +56,12 @@ public class BufferScreen implements Screen  {
             }
         });
 
-        stage.addActor(readyButton);
         stage.addActor(letsbuildButton);
     }
 
     @Override
     public void render(float delta) {
-        Gdx.gl.glClearColor(0/255f, 0/255f, 128/255f, 100/255f);
+        Gdx.gl.glClearColor(4/255f, 105/255f, 30/255f, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         stage.setViewport(gameport);
         stage.draw();
