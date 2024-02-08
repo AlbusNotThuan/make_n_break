fec72cf9bb4e43c3a4f7dfa9bce14204707100a7
AlbusNotThuan <107522443+AlbusNotThuan@users.noreply.github.com>
2024-02-08 15:50:39 +0100

    tidy up

--- core/src/Classes/Grid.java
+++ core/src/Classes/Grid.java
@@ -14,19 +14,21 @@ public class Grid extends Table {
     private final CustomButton[][] buttons;
     public final int COLNUM = 6;
     public final int ROWNUM = 7;
-    public Grid(){
+
+    public Grid() {
         super();
         buttons = new CustomButton[ROWNUM][COLNUM]; // Initialize the array
 
         // Create a 6x6 grid of buttons
         for (int i = 0; i < ROWNUM; i++) {
             for (int j = 0; j < COLNUM; j++) {
-                buttons[i][j] = new CustomButton(this,i*ROWNUM+j);
+                buttons[i][j] = new CustomButton(this, i * ROWNUM + j);
                 add(buttons[i][j]).width(50).height(50).pad(5);
             }
             row();
         }
     }
+
     public CustomButton getButton(int row, int col) {
         if (row >= 0 && row < buttons.length && col >= 0 && col < buttons[row].length) {
             return buttons[row][col];
@@ -34,8 +36,9 @@ public class Grid extends Table {
             throw new IllegalArgumentException("Invalid row or column");
         }
     }
+
     public boolean checkMatrix(String filepath) throws FileNotFoundException {
-        ArrayList<String> currentGrid = new ArrayList<String>();
+        ArrayList<String> currentGrid = new ArrayList<>();
         ArrayList<String> answer = null;
         try {
             answer = Text2Array(filepath);
@@ -43,10 +46,9 @@ public class Grid extends Table {
             e.printStackTrace();
             return false; // Return false or handle the exception as needed
         }
-        for (CustomButton[] row : buttons){
-            for (CustomButton button : row){
+        for (CustomButton[] row : buttons) {
+            for (CustomButton button : row) {
                 currentGrid.add(button.getSelectedColor().toString());
-//                System.out.println(button.selectedColor.toString());
             }
         }
         return currentGrid.equals(answer);
