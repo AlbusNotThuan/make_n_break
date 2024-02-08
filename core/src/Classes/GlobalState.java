d7e15a79acc56e9dd5f5bb1bf717fc2ce6fa05cc
AlbusNotThuan <107522443+AlbusNotThuan@users.noreply.github.com>
2024-02-08 12:12:29 +0100

    GlobalState remove unused methods

--- core/src/Classes/GlobalState.java
+++ core/src/Classes/GlobalState.java
@@ -4,10 +4,4 @@ import com.badlogic.gdx.graphics.Color;
 
 public class GlobalState {
     public static Color selectedColor = Color.YELLOW;
-    public Color getColor(){
-        return selectedColor;
-    }
-    public void setColor(Color color){
-        selectedColor = color;
-    }
 }
